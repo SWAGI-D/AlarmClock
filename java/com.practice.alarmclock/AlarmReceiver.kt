2b265525f9cfd8712347492f3508a5ee562b74c3
SWAGI-D <91810414+SWAGI-D@users.noreply.github.com>
2023-10-08 16:17:19 +0530

    Update AlarmReceiver.kt

--- java/com.practice.alarmclock/AlarmReceiver.kt
+++ java/com.practice.alarmclock/AlarmReceiver.kt
@@ -17,35 +17,35 @@ import android.os.Build
 class AlarmReceiver : BroadcastReceiver() {
 
 
- override fun onReceive(context: Context, intent: Intent) {
+    override fun onReceive(context: Context, intent: Intent) {
 
- val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
- val CHANNEL_ID = "my_channel_01"
- if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
- val name: CharSequence = "my_channel"
- val Description = "This is my channel"
- val importance = NotificationManager.IMPORTANCE_HIGH
- val mChannel = NotificationChannel(CHANNEL_ID, name, importance)
- mChannel.description = Description
- mChannel.enableLights(true)
- mChannel.enableVibration(true)
- mChannel.vibrationPattern = longArrayOf(100, 200, 300, 400, 500, 400, 300, 200, 400)
- mChannel.setShowBadge(false)
- notificationManager.createNotificationChannel(mChannel)
- }
+        val CHANNEL_ID = "my_channel_01"
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name: CharSequence = "my_channel"
+            val Description = "This is my channel"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val mChannel = NotificationChannel(CHANNEL_ID, name, importance)
+            mChannel.description = Description
+            mChannel.enableLights(true)
+            mChannel.enableVibration(true)
+            mChannel.vibrationPattern = longArrayOf(100, 200, 300, 400, 500, 400, 300, 200, 400)
+            mChannel.setShowBadge(false)
+            notificationManager.createNotificationChannel(mChannel)
+        }
 
- val dest_intent = Intent(context, DestinationActivity::class.java)
- dest_intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        val dest_intent = Intent(context, DestinationActivity::class.java)
+        dest_intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
 
- val pendingIntent = PendingIntent.getActivity(context, 0, dest_intent, 0)
+        val pendingIntent = PendingIntent.getActivity(context, 0, dest_intent, 0)
 
- val notification: Notification = NotificationCompat.Builder(context, CHANNEL_ID)
- .setSmallIcon(R.drawable.ic_dialog_alert)
- .setContentTitle(intent.getStringExtra("title"))
- .setContentText(intent.getStringExtra("text"))
- .setContentIntent(pendingIntent)
- .build()
- notificationManager.notify(42, notification)
- }
+        val notification: Notification = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_dialog_alert)
+            .setContentTitle(intent.getStringExtra("title"))
+            .setContentText(intent.getStringExtra("text"))
+            .setContentIntent(pendingIntent)
+            .build()
+        notificationManager.notify(42, notification)
+    }
 }
